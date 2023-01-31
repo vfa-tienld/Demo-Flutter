5e17046efd6ee6efd02698047062fad236c3a0fb
VFA-TienLD <an08011102@gmail.com>
2023-01-31 09:26:09 +0700

    demo_0_basic_dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,26 +2,95 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
+
+  //DATA TYPE
+
+  // Example 1: Integer type
+  int age = 30;
+  print("My age is $age");
+
+  // Example 2: Double type
+  double pi = 3.14;
+  print("The value of pi is $pi");
+
+  // Example 3: String type
+  String name = "John Doe";
+  print("My name is $name");
+
+  // Example 4: Boolean type
+  bool isTrue = true;
+  print("The value is $isTrue");
+
+  // Example 5: List type
+  List<int> numbers = [1, 2, 3];
+  print("The numbers are $numbers");
+
+// while
+  int count = 0;
+  while (count < 5) {
+    print(count);
+    count++;
+  }
+// do while
+  do {
+    print(count);
+    count++;
+  } while (count < 5);
+
+// if else , switch-case
+  String fruit = "apple";
+  switch (fruit) {
+    case "apple":
+      print("Apple is red.");
+      break;
+    case "banana":
+      print("Banana is yellow.");
+      break;
+    default:
+      if (fruit == "grape") {
+        print("Grape is purple.");
+      } else {
+        print("Unknown fruit.");
+      }
+      break;
+  }
+
+  //Collections  (loop)
+  List<String> fruits = ['apple', 'banana', 'grape'];
+  fruits.forEach((fruit) => print(fruit));
+
+//
+  Map<String, int> scores = {
+    'apple': 100,
+    'banana': 200,
+    'grape': 300,
+  };
+  scores.forEach((fruit, score) => print('$fruit: $score'));
+
+//
+  Set<String> fruits2 = {'apple', 'banana', 'grape'};
+  fruits.forEach((fruits2) => print(fruits2));
+
+//Null Safety
+
+  String name1 = "John Doe";
+  String? nickname1;
+
+// This line will result in a compile-time error, as `name` is not-nullable.
+  //name1 = null;
+
+// This line is allowed, as `nickname` is nullable.
+  nickname1 = null;
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -31,15 +100,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -52,47 +112,18 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
@@ -109,7 +140,7 @@ class _MyHomePageState extends State<MyHomePage> {
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
